9b87713e8b43ecbb85821e1bd3c11b2f9eb34ae9
Amit K Biswas <biswas1992@hotmail.com>
2018-01-30 17:29:02 -0500

    First Commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,27 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by MazeGeek on 1/17/18.
+//
+
+
+import Foundation
+
+class Question {
+    
+    var queationText : String = "am I real"
+    let answer : Bool
+    
+    init(text:String , corectAnswer: Bool){
+        
+       queationText = text
+         answer = corectAnswer
+        
+    }
+    
+  
+    
+}
+
+
